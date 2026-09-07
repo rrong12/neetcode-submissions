634f5437ec9d33eee5b6e33be8d6fdd84f5f01c4
rrong12 <rrong@uchicago.edu>
2026-09-07 02:09:52 -0400

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        # i * i <= x
+
+        l = 1
+        r = x
+        res = 0 
+
+        while l <= r: 
+            m = (l + r) // 2
+            if m * m == x:
+                return m
+            if m * m < x:
+                res = max(res, m)
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
